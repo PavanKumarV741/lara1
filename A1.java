0bd8c8b6f6260db416357d305438d81e3275c01a
PavanKumarV741 <pavankumarv741@gmail.com>
2023-09-19 16:59:47 +0530

    first commit

--- A1.java
+++ A1.java
@@ -3,5 +3,8 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("I am into git repo");
+		System.out.println("I am into git repo");
+		System.out.println("I am into git repo");
+		System.out.println("I am into git repo");
 	}
 }
